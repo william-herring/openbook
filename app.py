f0aacb920d53dcdb8bfa5b012ef8d91a7031f63c
williamherring <william.herring.au@gmail.com>
2024-07-23 11:32:05 +1000

    user authentication - registration, login, logout

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
@@ -9,22 +11,61 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+from models import *
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
 
-@app.route("/log-in")
+@app.route("/log-in", methods=['GET', 'POST'])
 def log_in():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/library')
+
     return render_template('log-in.html')
 
+@app.route("/sign-up", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-@app.route("/sign-up")
-def sign_up():
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/profile?editing=1')
+
     return render_template('sign-up.html')
+
+@app.route('/log-out')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/log-in')
 
 @app.route("/profile")
 def profile():
     return render_template('profile.html', user_name='William Herring', year_level=11)
+
+@app.route('/library')
+def library():
+    return render_template('index.html')
--- models.py
+++ models.py
@@ -1,14 +1,24 @@
 from datetime import datetime
+from app import bcrypt
+from flask_login import UserMixin
 from app import db
 
 # TODO: Many-to-many for users-books and books-authors
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    display_name = db.Column(db.String(200))
     avatar = db.Column(db.String(500), default="https://hostedboringavatars.vercel.app/api/beam")
-    role = db.Column(db.String(50), default="standard")
-    saved_books = db.relationship('Textbook', backref='user', lazy=True)
+    role = db.Column(db.String(50), default="student")
+    created_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        pwhash = bcrypt.generate_password_hash(password)
+        self.password = pwhash.decode('utf8')
+        self.created_on = datetime.now()
 
     def __repr__(self):
         return f"<User {self.id}>"
@@ -18,7 +28,6 @@ class Textbook(db.Model):
     last_updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
     book_code = db.Column(db.String(5), nullable=False)
     title = db.Column(db.String(100), nullable=False)
-    authors = db.relationship('Author', backref='textbook', lazy=True)
     pages = db.Column(db.Integer, nullable=False)
     repository = db.Column(db.String(500), nullable=False)
     pdf = db.Column(db.String(500), nullable=False)
