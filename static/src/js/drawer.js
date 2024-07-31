f1636363014ef74cfdd1757d9fec9b0f12b8f520
williamherring <william.herring.au@gmail.com>
2024-07-31 16:50:19 +1000

    working sidebar

---
+++ static/src/js/drawer.js
@@ -0,0 +1,7 @@
+function closeDrawer() {
+    document.getElementById('drawer').style.display = 'none';
+}
+
+function openDrawer() {
+    document.getElementById('drawer').style.display = 'block';
+}
