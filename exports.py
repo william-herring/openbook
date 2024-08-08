86e3fd380f338351202a3d7c0e9b425ca278706a
William Herring <73984595+william-herring@users.noreply.github.com>
2024-08-09 07:44:56 +1000

    added textbook styles

--- exports.py
+++ exports.py
@@ -1,10 +1,11 @@
-import aspose.words as aw
+import weasyprint
 
 def export_pdf(repository: str):
-    output = aw.Document()
-    output.remove_all_children()
-
+    weasyprint.HTML('https://raw.githack.com/' + repository[18:] + '/release/out/html/1.html').write_pdf('./uploads/textbook.pdf', stylesheets=[weasyprint.CSS('./static/src/css/textbook.css')])
     # collect html files
     # add each to a document and append to output
     # save output pdf to server
     # return upload location
+
+if __name__ == '__main__':
+    export_pdf('https://github.com/william-herring/example-textbook')
