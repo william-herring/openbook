a784bfd24f41e7d04ef6122423c70b7a2f0c540c
William Herring <73984595+william-herring@users.noreply.github.com>
2024-08-14 19:07:03 +1000

    INTERACTIVE QUESTIONS WOOHOOO

--- static/src/js/reader.js
+++ static/src/js/reader.js
@@ -5,3 +5,17 @@ function showDownloadPrompt() {
 function closeDownloadPrompt() {
     document.getElementById('download-prompt').style.display = 'none';
 }
+
+function showChaptersList() {
+    document.getElementById('questions-toggle').className = 'font-medium py-2 px-5 rounded-full hover:bg-black hover:bg-opacity-10';
+    document.getElementById('chapters-toggle').className = 'font-medium bg-black py-2 px-5 text-white rounded-full';
+    document.getElementById('questions-list').className = 'hidden';
+    document.getElementById('chapters-list').className = 'flex flex-col space-y-4';
+}
+
+function showQuestionsList() {
+    document.getElementById('chapters-toggle').className = 'font-medium py-2 px-5 rounded-full hover:bg-black hover:bg-opacity-10';
+    document.getElementById('questions-toggle').className = 'font-medium bg-black py-2 px-5 text-white rounded-full';
+    document.getElementById('chapters-list').className = 'hidden';
+    document.getElementById('questions-list').className = 'flex flex-col space-y-4';
+}
