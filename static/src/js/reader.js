4e06fb07efdce0f8ea11796472f7e117ce918c3a
William Herring <73984595+william-herring@users.noreply.github.com>
2024-08-15 18:15:42 +1000

    minor stuff

--- static/src/js/reader.js
+++ static/src/js/reader.js
@@ -11,6 +11,7 @@ function showChaptersList() {
     document.getElementById('chapters-toggle').className = 'font-medium bg-black py-2 px-5 text-white rounded-full';
     document.getElementById('questions-list').className = 'hidden';
     document.getElementById('chapters-list').className = 'flex flex-col space-y-4';
+    history.pushState({}, document.title, window.location.pathname);
 }
 
 function showQuestionsList() {
@@ -18,4 +19,7 @@ function showQuestionsList() {
     document.getElementById('questions-toggle').className = 'font-medium bg-black py-2 px-5 text-white rounded-full';
     document.getElementById('chapters-list').className = 'hidden';
     document.getElementById('questions-list').className = 'flex flex-col space-y-4';
+    const params = new URLSearchParams(window.location.search);
+    params.set('mode', 'questions');
+    history.pushState(null, null, '?' + params.toString());
 }
