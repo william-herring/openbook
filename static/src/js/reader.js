1c18ac41414e42b616ac6bb259dbff26c76293da
williamherring <william.herring.au@gmail.com>
2024-08-16 08:31:51 +1000

    temp

--- static/src/js/reader.js
+++ static/src/js/reader.js
@@ -10,12 +10,12 @@ function showChaptersList() {
     document.getElementById('questions-toggle').className = 'font-medium py-2 px-5 rounded-full hover:bg-black hover:bg-opacity-10';
     document.getElementById('chapters-toggle').className = 'font-medium bg-black py-2 px-5 text-white rounded-full';
     document.getElementById('questions-list').className = 'hidden';
-    document.getElementById('chapters-list').className = 'flex flex-col space-y-4';
+    document.getElementById('chapters-list').className = 'flex flex-col space-y-4 h-full overflow-y-scroll';
 }
 
 function showQuestionsList() {
     document.getElementById('chapters-toggle').className = 'font-medium py-2 px-5 rounded-full hover:bg-black hover:bg-opacity-10';
     document.getElementById('questions-toggle').className = 'font-medium bg-black py-2 px-5 text-white rounded-full';
     document.getElementById('chapters-list').className = 'hidden';
-    document.getElementById('questions-list').className = 'flex flex-col space-y-4';
+    document.getElementById('questions-list').className = 'flex flex-col space-y-4 h-full overflow-y-scroll';
 }
