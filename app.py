f881172c60721e8d6140b4a660c71d964568c847
William Herring <73984595+william-herring@users.noreply.github.com>
2024-08-21 07:53:35 +1000

    commented all python files

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 import textbooks
 import exports
 
+# Configuration
 load_dotenv()
 UPLOAD_FOLDER = './uploads'
 
@@ -25,7 +26,7 @@ login_manager.init_app(app)
 from models import *
 
 with app.app_context():
-    db.create_all()
+    db.create_all()  # Create tables
     db.session.commit()
 
 @app.route("/")
@@ -36,16 +37,16 @@ def index():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.filter(User.id == int(user_id)).first()
+    return User.query.filter(User.id == int(user_id)).first()  # This isn't really used but a useful tool for admin
 
 @app.route("/log-in", methods=['GET', 'POST'])
 def log_in():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email).first()  # Match the user with the email
 
-        if user and check_password_hash(user.password, password):
+        if user and check_password_hash(user.password, password):  # Check the password hash
             login_user(user)
             return redirect('/library')
 
@@ -57,6 +58,7 @@ def register():
         email = request.form['email']
         password = request.form['password']
 
+        # Create a user with credentials
         user = User(email=email, password=password)
         db.session.add(user)
         db.session.commit()
@@ -76,7 +78,7 @@ def logout():
 @app.route('/profile')
 @login_required
 def profile():
-    editing = request.args.get('editing') == '1'
+    editing = request.args.get('editing') == '1'  # This stores the editing state
     return render_template('profile.html', editing=editing, user_name=current_user.display_name, avatar=current_user.avatar, year_level=current_user.year_level)
 
 @app.route('/update-profile', methods=['POST'])
@@ -86,6 +88,7 @@ def update_profile():
     user = current_user
     avatar = data['avatar']
     display_name = data['display-name']
+    # There is no support for schools or classes in the app but the field exists, so it can possibly be implemented in the future
     # school = data['school']
     year_level = data['year-level']
 
@@ -99,23 +102,24 @@ def update_profile():
 @app.route('/library')
 def library():
     all_textbooks = Textbook.query.all()
-    recent_textbook = Textbook.query.filter_by(book_code=request.cookies.get('recent-textbook').split('/')[0]).first()
+    recent_textbook = Textbook.query.filter_by(book_code=request.cookies.get('recent-textbook').split('/')[0]).first()  # The recent textbook is stored in a cookie
     return render_template('library.html', avatar=current_user.avatar, all_textbooks=all_textbooks, recent_textbook=recent_textbook)
 
 @app.route('/textbook/<string:book_code>/<int:book_id>')
 def textbook_view(book_code, book_id):
     textbook = Textbook.query.filter_by(book_code=book_code, id=book_id).first()
-    recent_textbook = f'{textbook.book_code}/{textbook.id}'
+    recent_textbook = f'{textbook.book_code}/{textbook.id}'  # By storing a sliced version of the recent textbook's URL, it's easy to work with
     build_data = textbooks.get_textbook_build_data(textbook.repository)
     page_number = int(request.args.get('page')) if request.args.get('page') is not None else 0
-    show_questions = request.args.get('mode') == 'questions'
+    show_questions = request.args.get('mode') == 'questions'  # When switched to the questions tab, there is no page reload but the URL is modified to add the ?mode=questions argument so the tab selection will persist if there is a page reload
     resp = make_response(render_template('reader.html', avatar=current_user.avatar, textbook=textbook, build_data=build_data, page=page_number, show_questions=show_questions))
-    resp.set_cookie('recent-textbook', recent_textbook)
+    resp.set_cookie('recent-textbook', recent_textbook)  # Set recent textbook
     return resp
 
 @app.route('/upload-centre')
 @login_required
 def upload_centre():
+    # Only admins can see pending submissions
     if current_user.role == 'admin':
         pending_submissions = TextbookSubmission.query.all()
         return render_template('upload-centre.html', admin=True, pending=pending_submissions)
@@ -127,6 +131,7 @@ def submit_textbook():
     repository = request.form.get('repository')
     metadata = textbooks.get_textbook_options(repository)['metadata']
 
+    # Textbook submissions are their own separate table because there is no need to compile and store an entire textbook if it is yet to be approved
     submission = TextbookSubmission(
         title=metadata['title'],
         repository=repository
@@ -139,6 +144,7 @@ def submit_textbook():
 @app.route('/delete-submission', methods=['POST'])
 @login_required
 def delete_submission():
+    # This is an admin-only endpoint
     if current_user.role == 'admin':
         submission_id = request.json['submission-id']
         submission = TextbookSubmission.query.filter(TextbookSubmission.id == submission_id).first()
@@ -150,14 +156,17 @@ def delete_submission():
 @app.route('/approve-submission', methods=['POST'])
 @login_required
 def approve_submission():
+    # Collect all basic metadata
     repository = request.json['repository']
     submission_id = request.json['submission-id']
     options = textbooks.get_textbook_options(repository)
     cover_colour = options['book']['cover']['colour']
     title = options['metadata']['title']
     book_code = textbooks.generate_book_code(title)
+    # Generate a PDF
     pdf = exports.export_pdf(repository, book_code)
 
+    # Cycle through authors and create an instance if one doesn't exist
     authors = []
     for a in options['metadata']['authors']:
         q = Author.query.filter_by(name=a).all()
@@ -169,8 +178,10 @@ def approve_submission():
             author = q[0]
         authors.append(author)
 
+    # Get HTML
     html = textbooks.get_textbook_html_url(repository)
 
+    # Add to database
     textbook = Textbook(title=title, repository=repository, book_code=book_code, pdf=pdf, html=html, authors=authors) if cover_colour == 'default' else Textbook(title=title, repository=repository, book_code=book_code, pdf=pdf, html=html, authors=authors, cover_colour=cover_colour)
     db.session.add(textbook)
     submission = TextbookSubmission.query.filter_by(id=submission_id).first()
@@ -181,4 +192,5 @@ def approve_submission():
 
 @app.route('/uploads/<name>')
 def download_file(name):
+    # This endpoint exists for file access
     return send_from_directory(app.config['UPLOAD_FOLDER'], name)
--- exports.py
+++ exports.py
@@ -1,7 +1,10 @@
 import weasyprint
 
-
 def export_pdf(repository: str, book_code: str):
+    """
+    The PDF export function uses the Weasyprint library to convert a single HTML file created by the textbook engine
+    to a PDF file with styles. It is then added to the uploads folder as [book_code].pdf
+    """
     weasyprint.HTML('https://raw.githack.com/' + repository[18:] + '/release/out/html/pdftemplate.html').write_pdf(
         f'./uploads/{book_code}.pdf')
     return f'{book_code}.pdf'
